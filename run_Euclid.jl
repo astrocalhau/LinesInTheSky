c0ffd7f68549343e290a85a2d24e68422cc03933
Joao Calhau <joao.feiocalhau@inaf.it>
2026-04-01 22:16:47 +0100

    update to prevent host template from being fitted at z>=2.6

--- run_Euclid.jl
+++ run_Euclid.jl
@@ -62,19 +62,23 @@ function analyze_spec(input_path, output_path, row; clob=false)
     recipe.use_host_template = true
     resWithHost = analyze(recipe, spec)
 
-
-    if !(:QSOcont in keys(resNoHost.post[:Issues]))  &&
-       !(:QSOcont in keys(resWithHost.post[:Issues]))
-        if resNoHost.fsumm.fitstat < resWithHost.fsumm.fitstat
-            res = resNoHost
-        else
-            res = resWithHost
-        end
-    else # simply pick the one with no QSOcont issues
-        if !(:QSOcont in keys(resNoHost.post[:Issues]))
-            res = resNoHost
-        else
-            res = resWithHost
+    
+    if row[:Z]>=2.6
+        res = resNoHost
+    else
+        if !(:QSOcont in keys(resNoHost.post[:Issues]))  &&
+           !(:QSOcont in keys(resWithHost.post[:Issues]))
+            if resNoHost.fsumm.fitstat < resWithHost.fsumm.fitstat
+                res = resNoHost
+            else
+                res = resWithHost
+            end
+        else # simply pick the one with no QSOcont issues
+            if !(:QSOcont in keys(resNoHost.post[:Issues]))
+                res = resNoHost
+            else
+                res = resWithHost
+            end
         end
     end
 
